f7b92ba90278eef60eb331b22ce9fe7265f8399a
Nicolas Yarosz <nick.yarosz@gmail.com>
2020-07-02 16:48:34 -0700

    Clone other modules

--- server.js
+++ server.js
@@ -1,9 +1,50 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
-// const Desc = require('../database/model.js'); // <- need?
+const Desc = require('../description/database/model.js'); // <- need?
 
-const app = express();
+const proxy = express();
 const port = 3000;
 
-app.listen(port, () => (console.log(`proxy listening at: http://localhost:${port}`)));
+const description = express();
+const descriptionPort = 3006
+
+const photos = express();
+const photosPort = 3009
+
+const pledges = express();
+const pledgesPort = 3003;
+
+proxy.use(express.static(path.join(__dirname, './public')));
+
+description.use(express.static(path.join(__dirname, '../description/public')));
+
+proxy.use(morgan('tiny'));
+
+proxy.get('/', (req, res) => res.send('Hello Proxy!'))
+proxy.listen(port, () => (console.log(`proxy listening at: http://localhost:${port}`)));
+
+description.get('/', (req, res) => res.send('Hello Description!'))
+description.listen(descriptionPort, () => (console.log(`description listening at: http://localhost:${descriptionPort}`)));
+
+photos.get('/', (req, res) => res.send('Hello Photos!'))
+photos.listen(photosPort, () => (console.log(`photos listening at: http://localhost:${photosPort}`)));
+
+pledges.get('/', (req, res) => res.send('Hello Pledges!'))
+pledges.listen(pledgesPort, () => (console.log(`pledges listening at: http://localhost:${pledgesPort}`)));
+
+
+// returns all description data
+description.get('/api/description', (req, res) => {
+  Desc.find((err, desc) => {
+    res.send(desc);
+  });
+});
+
+// returns description at id
+description.get('/api/description/:id', (req, res) => {
+  Desc.find({ id: req.params.id}, (err, desc) => {
+    res.send(desc);
+  });
+});
+
